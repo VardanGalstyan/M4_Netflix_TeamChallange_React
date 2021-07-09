72e5f7743939f32f233de89ada0fec4d29030464
VardanGalstian <81989128+VardanGalstyan@users.noreply.github.com>
2021-07-09 16:02:59 +0200

    Latest Updates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import TopNavbar from './components/TopNavbar';
+import TopNavbar from './components/TopNavbar'
 import UnderNavbar from './components/UnderNavbar';
 import MainCarousel from './components/MainCarousel';
 import NetFooter from './components/NetFooter';
 import CarouselTwo from './components/CarouselTwo';
 import CarouselThree from './components/CarouselThree';
 
+
 function App() {
   return (
     <div className="App">
       <TopNavbar/>
       <UnderNavbar/>
-      <MainCarousel />
+      <MainCarousel/>
       <CarouselTwo />
       <CarouselThree />
       <NetFooter/>
