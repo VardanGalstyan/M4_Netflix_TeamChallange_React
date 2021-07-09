b5dad8da9b5f74b0fc7755364e099af39ce9a75f
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-07-09 12:10:37 +0200

    Carousel

--- src/components/MainCarousel.jsx
+++ src/components/MainCarousel.jsx
@@ -1,11 +1,45 @@
+
 import React, { Component } from 'react'
+import { CarouselItem, Container, Row, Col, Carousel, Card } from 'react-bootstrap'
 
 export default class MainCarousel extends Component {
+    state = {
+        films: []
+    }
+
+    componentDidMount = async () => {
+        let response = await fetch("http://www.omdbapi.com/?s=starwars&apikey=72db6b6a")
+        let res = await response.json()
+        console.log(res)
+        this.setState({
+            films: res.Search
+        })
+    }
+
     render() {
         return (
-            <div>
-                <h1>Carousel Here</h1>
-            </div>
+            
+            <Container>
+                <Carousel>
+                    <CarouselItem>
+                      <Row className="row-cols-sm-6 row">
+                        {
+                                this.state.films.map(film => <Col key={film.imdbID} className="col mb-4">
+                                    <Card><img
+                                        className="card-img-top"
+                                        src={film.Poster}
+                                        alt="First slide"
+                                        />
+                                    </Card>
+                                </Col>)
+                            }
+                            </Row>
+                    </CarouselItem>
+                </Carousel>
+            </Container>
+
+
+
         )
     }
 }
